bcb65e917f56f8501ccc08b90433097be977aca4
Caio <caiodib10@gmail.com>
2025-06-07 20:07:11 -0300

    ✨ feat  adiciona feedback de  carregamento e tratamento de erro

---
+++ PokedexApp/screens/PokedexScreen.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, TextInput, StyleSheet, ActivityIndicator } from 'react-native';
+import { getPokemons, getPokemonDetails } from '../services/api';
+import { Pokemon } from '../types/Pokemon';
+import { PokemonCard } from '../components/PokemonCard';
+
+export const PokedexScreen = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [search, setSearch] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        setError('');
+
+        const list = await getPokemons(30);
+        const details = await Promise.all(list.map(p => getPokemonDetails(p.url)));
+        setPokemons(details);
+
+      } catch (err) {
+        setError('Falha ao carregar Pokémons. Verifique sua conexão.');
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const filtered = pokemons.filter(p => p.name.includes(search.toLowerCase()));
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Carregando Pokémons...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.centered}>
+        <Text>{error}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Pokédex</Text>
+      <TextInput
+        placeholder="Buscar pokémon..."
+        style={styles.input}
+        onChangeText={setSearch}
+      />
+      <FlatList
+        data={filtered}
+        keyExtractor={item => item.id.toString()}
+        numColumns={2}
+        renderItem={({ item }) => <PokemonCard pokemon={item} />}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, paddingTop: 60, paddingHorizontal: 16 },
+  title: { fontSize: 32, fontWeight: 'bold', marginBottom: 12 },
+  input: {
+    backgroundColor: '#f1f1f1',
+    padding: 10,
+    borderRadius: 8,
+    marginBottom: 20,
+  },
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
